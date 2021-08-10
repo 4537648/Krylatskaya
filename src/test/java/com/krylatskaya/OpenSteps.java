186a56980b7ba490ac6f0a752c237a5a18f6e055
4537648 <4537648@gmail.com>
2021-08-10 09:41:31 +0300

    commit

--- src/test/java/com/krylatskaya/OpenSteps.java
+++ src/test/java/com/krylatskaya/OpenSteps.java
@@ -1,11 +1,13 @@
 package com.krylatskaya;
 
 import io.cucumber.java.ru.*;
+
+import java.util.Base64;
+
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selectors.byXpath;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 import static com.krylatskaya.Hooks.pass;
 import static com.krylatskaya.Hooks.url;
 import static org.apache.commons.lang3.StringUtils.isNotBlank;
@@ -19,29 +21,31 @@ public class OpenSteps {
 
     @Допустим("Проверить наличие {string} внутри {string}")
     public void checkMenu(String string1, String string2) {
-        if (isNotBlank(string2)) {
-            $(byXpath("//*[text()='" + string2 + "']")).hover();
-        }
+        if (isNotBlank(string2)) {$(byXpath("//*[text()='" + string2 + "']")).hover();}
         $(byText(string1)).shouldBe(visible);
         System.out.println(">>> Checking " + string1);
     }
 
     @Допустим("Перейти в раздел {string}")
-    public void перейти_в_раздел(String string) {
+    public void changeToFolder(String string) {
         open(url + string);
         $(byText("Защищено: Деловой раздел")).shouldBe(visible);
     }
 
     @Допустим("Ввести пароль \\({string})")
-    public void ввести_пароль(String string) {
-        if (string == "успех") {$("input[type='password']").val(pass);}
-        if (string == "неуспех") {$("input[type='password']").val("12345678");}
-
+    public void enterPassword(String string) {
+        String open = string;
+        byte[] decodedBytes = Base64.getDecoder().decode(pass);
+        String decodedString = new String(decodedBytes);
+        if ("успех".equals(open)) {$("input[type='password']").val(decodedString);
+        } else if ("неуспех".equals(open)) {$("input[type='password']").val("12345678");}
     }
     @Допустим("Нажать на кнопку {string} \\({string})")
-    public void нажать_на_кнопку(String string, String string2) {
+    public void pushTheButton(String string, String string2) {
+        String open = string2;
+
         $("input[value="+ string +"]").click();
-        if (string2 == "успех") {$(byText("Важное")).shouldBe(visible);}
-        if (string2 == "неуспех") {$(byText("Защищено: Деловой раздел")).shouldBe(visible);}
+        if ("успех".equals(open)) {$(byText("Важное")).shouldBe(visible);
+        } else if ("неуспех".equals(open)) {$(byText("Защищено: Деловой раздел")).shouldBe(visible);}
     }
 }
